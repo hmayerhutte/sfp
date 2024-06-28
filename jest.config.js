9b21ffc0ab29d69342ea46c0ed706bdc6df1ff4b
azlam <sfopsbot@flxbl.io>
2024-06-29 00:17:36 +1000

    fix: mapping to test lib

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   testMatch: ['**/tests/**/*.test.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '^@salesforce/core/testSetup$': '<rootDir>/node_modules/@salesforce/core/lib/testSetup',
   },
 };
